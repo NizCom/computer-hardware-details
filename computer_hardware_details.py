bd9d05111968a0494d7ee6898e26efb731913182
NizCom <nizan.naor11@gmail.com>
2023-12-28 20:45:56 +0200

    first commit - uploading project

---
+++ computer_hardware_details.py
@@ -0,0 +1,10 @@
+import functions as f
+import functions_on_table as ft
+
+
+def main():
+    f.check_argv()
+    ft.print_computer_hardware_details()
+
+if __name__ == "__main__":
+    main()
---
+++ functions.py
@@ -0,0 +1,49 @@
+
+# Logical functions
+
+import sys
+
+# Global variables
+exponents_map = {'BYTES': 0, 'KB': 1, 'MB': 2, 'GB': 3, 'TB': 4}
+unit = 'GB'
+
+# Constants
+RED = "\033[1;31m"
+YELLOW = "\033[1;33m"
+GREEN = "\033[1;32m"
+DEFAULT = "\033[0m"  # Reset
+
+def check_argv():
+    if len(sys.argv) == 1:
+        unit = 'GB' # Default
+    elif check_unit_input(sys.argv[1]):
+        unit = sys.argv[1].upper()
+    else:
+        print("input is wrong! Must select from\
+        ['BYTES', 'KB', 'MB', 'GB', 'TB']")
+
+def check_unit_input():
+    global exponents_map, unit
+    unit_upper = unit.upper()
+
+    if unit_upper in exponents_map:
+        return True
+    else:
+        return False
+
+def size_format(size):
+    global exponents_map, unit
+
+    if unit not in exponents_map:
+        raise ValueError("Must select from   ['BYTES', 'KB', 'MB', 'GB', 'TB']")
+    else:
+        res = size / 1024 ** exponents_map[unit]
+        return str(round(res, 3)) + unit
+
+def get_font_color(percentage):
+    if percentage <= 25:
+        return GREEN
+    elif percentage <= 50:
+        return YELLOW
+    else:
+        return RED
